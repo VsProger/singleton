ea14d254cf171947bb2832bd3a0741774c708fd7
Baurzhan Saliyev <220476@astanait.edu.kz>
2023-10-31 20:11:16 +0600

    pattern singlton

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type single struct {
+}
+
+var (
+	once           sync.Once
+	singleInstance *single
+)
+
+func getInstance() *single {
+	once.Do(func() {
+		fmt.Println("Creating single instance now.")
+		singleInstance = &single{}
+	})
+	fmt.Println("Returning single instance.")
+	return singleInstance
+}
+
+func main() {
+	for i := 0; i < 30; i++ {
+		go getInstance()
+	}
+
+	fmt.Scanln()
+}
